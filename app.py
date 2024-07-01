e530e899c5dc4ce2d0a8830c59313a854141125d
leonardojgm <cookie.maia@gmail.com>
2024-07-01 11:24:55 -0300

    Implementação com assistente

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-
 from flask import Flask,render_template, request, Response
 from openai import OpenAI
 from dotenv import load_dotenv
-import os
 from time import sleep
 from helpers import *
 from selecionar_persona import *
 from selecionar_documento import *
 from assistente_ecomart import *
+import os
 
 load_dotenv()
 
@@ -15,11 +14,15 @@ cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = "gpt-4"
 
 app = Flask(__name__)
-
 app.secret_key = 'alura'
 
-assistente = criar_assistente()
-thread = criar_thread()
+assistente = pegar_json()
+thread_id = assistente["thread_id"]
+assistente_id = assistente["assistant_id"]
+file_ids = assistente["file_ids"]
+
+STATUS_COMPLETED = "completed" 
+STATUS_REQUIRES_ACTION = "requires_action" 
 
 def bot(prompt):
     maximo_tentativas = 1
@@ -27,38 +30,77 @@ def bot(prompt):
 
     while True:
         try:
+            personalidade = personas[selecionar_persona(prompt)]
+
             cliente.beta.threads.messages.create(
-                thread_id=thread.id, 
+                thread_id=thread_id, 
                 role = "user",
-                content =  prompt
+                content =  f"""
+                Assuma, de agora em diante, a personalidade abaixo. 
+                Ignore as personalidades anteriores.
+
+                # Persona
+                {personalidade}
+                """,
+                file_ids = file_ids
+            )
+            cliente.beta.threads.messages.create(
+                thread_id=thread_id, 
+                role = "user",
+                content =  prompt,
+                file_ids = file_ids
             )
             run = cliente.beta.threads.runs.create(
-                thread_id=thread.id,
-                assistant_id=assistente.id
+                thread_id = thread_id,
+                assistant_id = assistente_id
             )
 
-            while run.status !="completed":
+            while run.status != STATUS_COMPLETED:
                 run = cliente.beta.threads.runs.retrieve(
-                    thread_id=thread.id,
-                    run_id=run.id
-            )
+                    thread_id = thread_id,
+                    run_id = run.id
+                )
+
+                print(f"Status: {run.status}")
+                
+                if run.status == STATUS_REQUIRES_ACTION:
+                    tools_acionadas = run.required_action.submit_tool_outputs.tool_calls
+                    respostas_tools_acionadas = [] 
+
+                    for uma_tool in tools_acionadas:
+                        nome_funcao = uma_tool.function.name
+                        funcao_escolhida = minhas_funcoes[nome_funcao]
+                        argumentos = json.loads(uma_tool.function.arguments)
+
+                        print(argumentos)
+
+                        resposta_funcao = funcao_escolhida(argumentos)
+
+                        respostas_tools_acionadas.append({
+                                "tool_call_id": uma_tool.id,
+                                "output": resposta_funcao
+                            })
+                    
+                    run = cliente.beta.threads.runs.submit_tool_outputs(
+                            thread_id = thread_id,
+                            run_id = run.id,
+                            tool_outputs = respostas_tools_acionadas
+                        )
             
-            historico = list(cliente.beta.threads.messages.list(thread_id=thread.id).data)
+            historico = list(cliente.beta.threads.messages.list(thread_id=thread_id).data)
             resposta = historico[0]
 
             return resposta
 
         except Exception as erro:
-            repeticao += 1
+                repeticao += 1
 
-            if repeticao >= maximo_tentativas:
-                    return "Erro no GPT: %s" % erro
+                if repeticao >= maximo_tentativas:
+                        return "Erro no GPT: %s" % erro
+                
+                print('Erro de comunicação com OpenAI:', erro)
+                sleep(1)
             
-            print('Erro de comunicação com OpenAI:', erro)
-            sleep(1)
-        
-
-
 @app.route("/chat", methods=["POST"])
 def chat():
     prompt = request.json["msg"]
